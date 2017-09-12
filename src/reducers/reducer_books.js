0689a06842adfd3d48b7398ee455da781536d0f6
MarcoJr <marco.jr16@gmail.com>
2017-09-12 10:17:44 -0400

    Creation of book reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,12 @@
+export default function () {
+  return [
+    { title: 'To Kill a Mockingbird'},
+    { title: '1984'},
+    { title: 'Harry Potter and the Philosopher’s Stone'},
+    { title: 'The Lord of the Rings'},
+    { title: 'he Great Gatsby'},
+    { title: 'Pride and Prejudice'}
+  ]
+}
+/* este reducer retorna solo retorna un pedazo del estado
+ en este caso "books"*/
