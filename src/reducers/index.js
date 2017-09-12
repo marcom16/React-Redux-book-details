c2751b01a7c0eecefabee4b304ad89024b67ae77
MarcoJr <marco.jr16@gmail.com>
2017-09-12 10:18:09 -0400

    Calling the books reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux'
+import BooksReducer from './reducer_books' //Retorna el valor del reducer
 
+/*aqui en el index se combinan los reducers*/
 const rootReducer = combineReducers({
-  state: (state = {}) => state
-});
+  books: BooksReducer /*Se guarda lo retornado en reducer_books
+                        El reducer se llama de manera "capitalize"
+                        Mayusculas al principio siempre*/
+})
 
-export default rootReducer;
+export default rootReducer
