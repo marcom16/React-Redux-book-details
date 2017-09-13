9f3573cda0fbb693b26f493652ade3dba1f91dd1
MarcoJr <marco.jr16@gmail.com>
2017-09-13 09:58:32 -0400

    Show the book details

---
+++ src/containers/book-detail.js
@@ -0,0 +1,36 @@
+import React,{Component} from 'react';
+import { connect } from 'react-redux' /* Esta libreria espara unir las dos librerias*/
+
+class BookDetail extends Component {
+    render() {
+      if (!this.props.book) {
+        return <div>Select a book to get started</div>
+      }
+
+      return (
+            <div className="col">
+                <div className="card border-dark mb-3">
+                  <div className="card-body">
+                    <h4 className="card-title">{this.props.book.title}</h4>
+                  </div>
+                  <ul className="list-group list-group-flush">
+                    <li className="list-group-item">Author: {this.props.book.author}</li>
+                    <li className="list-group-item">Pages: {this.props.book.pages}</li>
+                  </ul>
+
+                  <div className="card-body">
+                    <p className="card-text">{this.props.book.desc}</p>
+                  </div>
+                </div>
+            </div>
+      );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+      book: state.activeBook
+    }
+}
+
+export default connect(mapStateToProps)(BookDetail)
