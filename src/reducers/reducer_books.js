25d1e29976373167ecba2d0c7305fcc0490cd181
MarcoJr <marco.jr16@gmail.com>
2017-09-13 09:58:52 -0400

    Adding information

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,11 +1,35 @@
 export default function () {
   return [
-    { title: 'To Kill a Mockingbird', pages: '125'},
-    { title: '1984', pages: '352'},
-    { title: 'Harry Potter and the Philosopher’s Stone', pages: '365'},
-    { title: 'The Lord of the Rings', pages: '236'},
-    { title: 'he Great Gatsby', pages: '652'},
-    { title: 'Pride and Prejudice', pages: '365'}
+    { title: 'To Kill a Mockingbird',
+      author: 'Harper Lee',
+      desc: 'To Kill a Mockingbird is primarily a novel about growing up under extraordinary circumstances in the 1930s in the Southern United States. The story covers a span of three years, during which the main characters undergo significant changes.',
+      pages: '125'
+    },
+    { title: '1984',
+      author: 'George Orwell',
+      desc:  'In George Orwells 1984, Winston Smith wrestles with oppression in Oceania, a place where the Party scrutinizes human actions with ever-watchful Big Brother. Defying a ban on individuality, Winston dares to express his thoughts in a diary and pursues a relationship with Julia. These criminal deeds bring Winston into the eye of the opposition, who then must reform the nonconformist. George Orwells 1984 introduced the watchwords for life without freedom: BIG BROTHER IS WATCHING YOU.',
+      pages: '352'
+    },
+    { title: 'Harry Potter and the Philosopher’s Stone',
+      author: 'J. K. Rowling',
+      desc: 'Harry Potter and the Philosophers Stone is the first novel in the Harry Potter series and J. K. Rowlings debut novel, first published in 1997 by Bloomsbury. It was published in the United States as Harry Potter and the Sorcerers Stone by Scholastic Corporation in 1998. The plot follows Harry Potter, a young wizard who discovers his magical heritage as he makes close friends and a few enemies in his first year at the Hogwarts School of Witchcraft and Wizardry. With the help of his friends, Harry faces an attempted comeback by the dark wizard Lord Voldemort, who killed Harrys parents, but failed to kill Harry when he was just 15 months old.',
+      pages: '365'
+    },
+    { title: 'The Lord of the Rings',
+      author: 'J. R. R. Tolkien',
+      desc: `The Lord of the Rings is an epic high fantasy novel written by English author and scholar J. R. R. Tolkien. The story began as a sequel to Tolkien's 1937 fantasy novel The Hobbit, but eventually developed into a much larger work. Written in stages between 1937 and 1949, The Lord of the Rings is one of the best-selling novels ever written, with over 150 million copies sold. \n The title of the novel refers to the story's main antagonist, the Dark Lord Sauron, who had in an earlier age created the One Ring to rule the other Rings of Power as the ultimate weapon in his campaign to conquer and rule all of Middle-earth.`,
+      pages: '236'
+    },
+    { title: 'The Great Gatsby',
+      author: 'F. Scott Fitzgerald',
+      desc: `The Great Gatsby is a 1925 novel written by American author F. Scott Fitzgerald that follows a cast of characters living in the fictional town of West Egg on prosperous Long Island in the summer of 1922. The story primarily concerns the young and mysterious millionaire Jay Gatsby and his quixotic passion and obsession for the beautiful former debutante Daisy Buchanan. Considered to be Fitzgerald's magnum opus, The Great Gatsby explores themes of decadence, idealism, resistance to change, social upheaval, and excess, creating a portrait of the Jazz Age or the Roaring Twenties that has been described as a cautionary tale regarding the American Dream`,
+      pages: '652'
+    },
+    { title: 'Pride and Prejudice',
+      author: '	Jane Austen',
+      desc: `Pride and Prejudice is a romance novel by Jane Austen, first published in 1813. The story charts the emotional development of the protagonist, Elizabeth Bennet, who learns the error of making hasty judgments and comes to appreciate the difference between the superficial and the essential. The comedy of the writing lies in the depiction of manners, education, marriage and money in the British Regency.`,
+      pages: '365'
+    }
   ]
 }
 /* este reducer retorna solo retorna un pedazo del estado
